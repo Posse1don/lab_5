0aa43dde1c3143d3e5e02512b45e8301509669e3
Posse1don <7985858@gmail.com>
2024-12-01 16:50:00 +0200

    Написав бажаний код реалізації, наступним кроком додамо тести

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,17 +1,25 @@
 package com.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Document doc = new TextDocument("This is a new document.");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Document encrypted = new EncryptedDocument(doc);
+        Document compressed = new CompressedDocument(encrypted);
+        Document watermarked = new WatermarkedDocument(compressed, "Confidential");
+
+        System.out.println("Final Content: " + watermarked.getContent());
+
+        DocumentLifecycle lifecycle = new DocumentLifecycle();
+        System.out.println("Initial State: " + lifecycle.getCurrentState());
+
+        lifecycle.moveToNextState();
+        System.out.println("State after review: " + lifecycle.getCurrentState());
+
+        lifecycle.moveToNextState();
+        System.out.println("State after approval: " + lifecycle.getCurrentState());
+
+        lifecycle.moveToNextState();
+        System.out.println("State after archival: " + lifecycle.getCurrentState());
     }
 }

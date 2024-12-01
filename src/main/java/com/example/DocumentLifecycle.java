a3f1e91cdd6c351aed56b6cf474581c9121df027
Posse1don <7985858@gmail.com>
2024-12-01 17:44:58 +0200

    клас для життєвого циклу документів

--- src/main/java/com/example/DocumentLifecycle.java
+++ src/main/java/com/example/DocumentLifecycle.java
@@ -1,4 +1,21 @@
 package com.example;
 
 public class DocumentLifecycle {
+    private DocumentState currentState;
+
+    public DocumentLifecycle() {
+        this.currentState = new DraftState();
+    }
+
+    public void setState(DocumentState state) {
+        this.currentState = state;
+    }
+
+    public void moveToNextState() {
+        currentState.moveToNext(this);
+    }
+
+    public String getCurrentState() {
+        return currentState.getStateName();
+    }
 }
